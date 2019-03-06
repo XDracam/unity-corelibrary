7d8947a3afc40aa5f73054a4c69dd8239abc9c5b
Daniel Götz <daniel.goetz1@stud-mail.uni-wuerzburg.de>
2019-03-06 12:48:39 +0100

    Added Object.SafeDestroy extension method. Added IEnumerable<T>.TakeWhile inclusive extension method

---
+++ src/CoreLibrary/Assets/Scripts/CoreLibrary/Base/ObjectExtensions.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace CoreLibrary{
+	/// <summary>
+	/// Author: Daniel Götz
+	/// <br/><br/>
+	/// This class provides useful extension methods for unity's Objects
+	/// </summary>
+	public static class ObjectExtensions
+	{
+		/// <summary>
+		/// This will always destroy the Object in the correct way, so that Unity will not throw an error
+		/// Note: This cannot be called in the OnValidateMethod
+		/// If your write code that is both executed in the editor while not playing and when playing and want to use Destroy, this method becomes useful:
+		/// Normally you have to check a preprocessor directive and if the application is playing to determine if you should use Destroy or DestroyImmediate
+		/// </summary>
+		public static void SafeDestroy(this Object o){
+	#if UNITY_EDITOR
+			if (EditorApplication.isPlaying)
+			{
+				Object.Destroy(o);
+			}
+			else
+			{
+				Object.DestroyImmediate(o);
+			}
+	#else
+			Destroy(o);
+	#endif
+		}  
+	}
+}
